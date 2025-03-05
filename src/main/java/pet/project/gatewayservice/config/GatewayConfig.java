e3afc990c4e0310243aec1ee74b4b6209f3d43e0
Edik <davydiantsedik@mail.ru>
2025-03-05 23:34:17 +0300

    GatewayConfig completed

--- src/main/java/pet/project/gatewayservice/config/GatewayConfig.java
+++ src/main/java/pet/project/gatewayservice/config/GatewayConfig.java
@@ -8,13 +8,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-    private final static String AUTH_SERVICE_ROUTE_ID = "auth_service_route_id";
+    private final static String AUTH_SERVICE_ROUTE_ID = "1";
     private final static String AUTH_SERVICE_PREFIX = "api/auth";
     private final static String AUTH_SERVICE_URI = "http://localhost:8081";
 
-    private final static String USER_SERVICE_ROUTE_ID = "user_service_route_id";
+    private final static String USER_SERVICE_ROUTE_ID = "2";
     private final static String USER_SERVICE_PREFIX = "api/users";
     private final static String USER_SERVICE_URI = "http://localhost:8082";
+
+    private final static String POST_SERVICE_ROUTE_ID = "3";
+    private final static String POST_SERVICE_PREFIX = "api/posts";
+    private final static String POST_SERVICE_URI = "http://localhost:8083";
+
+    private final static String NOTIFICATION_SERVICE_ROUTE_ID = "4";
+    private final static String NOTIFICATION_SERVICE_PREFIX = "api/notifications";
+    private final static String NOTIFICATION_SERVICE_URI = "http://localhost:8084";
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
@@ -25,6 +33,12 @@ public class GatewayConfig {
                 .route(USER_SERVICE_ROUTE_ID, r -> r.path(USER_SERVICE_PREFIX + "/**")
                         .filters(f -> f.stripPrefix(2))
                         .uri(USER_SERVICE_URI))
+                .route(POST_SERVICE_ROUTE_ID, r -> r.path(POST_SERVICE_PREFIX + "/**")
+                        .filters(f -> f.stripPrefix(2))
+                        .uri(POST_SERVICE_URI))
+                .route(NOTIFICATION_SERVICE_ROUTE_ID, r -> r.path(NOTIFICATION_SERVICE_PREFIX + "/**")
+                        .filters(f -> f.stripPrefix(2))
+                        .uri(NOTIFICATION_SERVICE_URI))
                 .build();
     }
 }
